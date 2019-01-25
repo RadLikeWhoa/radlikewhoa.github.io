899bd7b6deb70dad62a5638116abb6502a5751b7
Sacha Schmid <schmid.sacha@gmail.com>
2019-01-25 10:01:04 +0100

    Simplify mobile navigation

--- src/components/header.js
+++ src/components/header.js
@@ -5,72 +5,88 @@ import React from 'react'
 import logo from '../images/logo.svg'
 import logoLight from '../images/logo-light.svg'
 
-const Header = ({ siteTitle, alternate, highlightColor }) => (
-  <header className={`site-header ${alternate && 'is-alternate'}`}>
-    <div className="wrap">
-      <h1>
-        <Link to="/">
-          <img
-            className="site-logo"
-            src={alternate ? logoLight : logo}
-            alt="Logo" />
-        </Link>
-      </h1>
-  		<nav className="site-nav">
-  			<Link
-          to="/"
-          getProps={props => ({ className: `site-nav-item ${props.isCurrent ? 'is-active' : ''}` })}>
-          <span
-            className="label"
-            style={{ color: highlightColor }}>
-            Home
-          </span>
-          <span data-icon="home"></span>
-        </Link>
-  			<Link
-          to="/projects"
-          getProps={props => ({ className: `site-nav-item ${props.isCurrent || props.isPartiallyCurrent ? 'is-active' : ''}` })}>
-          <span
-            className="label"
-            style={{ color: highlightColor }}>
-            Projects
-          </span>
-          <span data-icon="case"></span>
-        </Link>
-  			<Link
-          to="/articles"
-          getProps={props => ({ className: `site-nav-item ${props.isCurrent || props.isPartiallyCurrent ? 'is-active' : ''}` })}>
-          <span
-            className="label"
-            style={{ color: highlightColor }}>
-            Articles
-          </span>
-          <span data-icon="archive"></span>
-        </Link>
-  			<Link
-          to="/about"
-          getProps={props => ({ className: `site-nav-item ${props.isCurrent || props.isPartiallyCurrent ? 'is-active' : ''}` })}>
-          <span
-            className="label"
-            style={{ color: highlightColor }}>
-            About
-          </span>
-          <span data-icon="person"></span>
-        </Link>
-  			<div className="active-nav-item"></div>
-  		</nav>
-    </div>
-  </header>
-)
+class Header extends React.Component {
+  state = {
+    open: false
+  }
+
+  render() {
+    const { alternate, highlightColor } = this.props
+    const { open } = this.state
+
+    return (
+      <header className={`site-header ${alternate && 'is-alternate'}`}>
+        <div className="wrap">
+          <h1>
+            <Link to="/">
+              <img
+                className="site-logo"
+                src={alternate ? logoLight : logo}
+                alt="Logo" />
+            </Link>
+          </h1>
+      		<nav
+            className={`site-nav ${open && 'is-open'}`}
+            onClick={() => this.setState({ open: false })}>
+      			<Link
+              to="/"
+              getProps={props => ({ className: `site-nav-item ${props.isCurrent ? 'is-active' : ''}` })}>
+              <span
+                className="label"
+                style={{ color: highlightColor }}>
+                Home
+              </span>
+              <span data-icon="home"></span>
+            </Link>
+      			<Link
+              to="/projects"
+              getProps={props => ({ className: `site-nav-item ${props.isCurrent || props.isPartiallyCurrent ? 'is-active' : ''}` })}>
+              <span
+                className="label"
+                style={{ color: highlightColor }}>
+                Projects
+              </span>
+              <span data-icon="case"></span>
+            </Link>
+      			<Link
+              to="/articles"
+              getProps={props => ({ className: `site-nav-item ${props.isCurrent || props.isPartiallyCurrent ? 'is-active' : ''}` })}>
+              <span
+                className="label"
+                style={{ color: highlightColor }}>
+                Articles
+              </span>
+              <span data-icon="archive"></span>
+            </Link>
+      			<Link
+              to="/about"
+              getProps={props => ({ className: `site-nav-item ${props.isCurrent || props.isPartiallyCurrent ? 'is-active' : ''}` })}>
+              <span
+                className="label"
+                style={{ color: highlightColor }}>
+                About
+              </span>
+              <span data-icon="person"></span>
+            </Link>
+      			<div className="active-nav-item"></div>
+      		</nav>
+          <button
+            className="nav-toggle"
+            onClick={() => this.setState({ open: true })}>
+            <span data-icon="list"></span>
+          </button>
+        </div>
+      </header>
+    )
+  }
+}
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
   alternate: PropTypes.bool,
   highlightColor: PropTypes.string
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
   alternate: false,
   highlightColor: '#fff'
 }
